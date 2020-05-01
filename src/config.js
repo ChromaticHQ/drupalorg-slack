1d9527c8574512d7d255ca2f466620f5af90436b
Glitch (chq-drupal-org) <none>
2020-05-01 18:08:23 +0000

    💿🌓 Updated with Glitch

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ module.exports = {
   debugMode: process.env.DEBUG_MODE === 'true',
   slackBotToken: process.env.SLACK_BOT_TOKEN,
   slackSigningSecret: process.env.SLACK_SIGNING_SECRET,
-  chromaticToken: process.env.CHROMATIC_TOKEN,
+  orgToken: process.env.ORG_TOKEN,
   channels: {
-    announcementsId: 'C03FBG24G',
-    openSourceId: 'C0J8SLT1V',
+    defaultChannelId: process.env.DEFAULT_CHANNEL_ID,
     sandboxId: process.env.SANDBOX_CHANNEL_ID,
   },
-  chromaticDrupalOrgNid: 2127245,
+  drupalOrganizationNodeId: process.env.DRUPAL_ORG_NODE_ID,
   drupalOrgBaseUrl: 'https://www.drupal.org',
   drupalOrgMarketplacePath: '/drupal-services',
+  slackNotificationText: process.env.SLACK_NOTIFICATION_TEXT,
 };
--- src/server.js
+++ src/server.js
@@ -14,18 +14,28 @@ const app = new App({
   logLevel: LogLevel.DEBUG,
 });
 
+/**
+ * Navigate Drupal.org marketplace pages until the specified
+ * organization is found, then calculates the organization's
+ * rank and return it.
+ *
+ * @param {marketplaceUrl} string URL of marketplace URL to
+ *   search for the orgnization.
+ * @param {rankCount} int A running counter for determining
+ *   an organization's rank across multiple pages.
+ * @return {int} Whether something occurred.
+ */
 function getDrupalMarketplaceData(marketplaceUrl, rankCount = 0) {
   return axios.get(marketplaceUrl)
     .then((marketplaceResponse) => {
       const html = marketplaceResponse.data;
       const $ = cheerio.load(html, { xmlMode: false });
       console.log($('.view-drupalorg-organizations .view-content').children().length);
-      console.log($(`#node-${config.chromaticDrupalOrgNid} h2`).text());
+      console.log($(`#node-${config.drupalOrganizationNodeId} h2`).text());
       // Determine if Chromatic is listed on the current page.
-      if ($(`#node-${config.chromaticDrupalOrgNid}`).length) {
-        console.log('FOUND');
+      if ($(`#node-${config.drupalOrganizationNodeId}`).length) {
         // Find the position of Chromatic on the page.
-        const foundRank = rankCount + $(`#node-${config.chromaticDrupalOrgNid}`).parent().prevAll().length + 1;
+        const foundRank = rankCount + $(`#node-${config.drupalOrganizationNodeId}`).parent().prevAll().length + 1;
         return foundRank;
       }
       // Chromatic is not on the current page, go to the next page.
@@ -50,7 +60,7 @@ const drupalOrgPayloadBlocks = (
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: 'Chromatic <https://drupal.org/chromatic|drupal.org> Statistics :chromatic::zap::drupal:',
+      text: config.slackNotificationText,
     },
   });
   blocks.push({
@@ -96,7 +106,7 @@ const drupalOrgPayloadBlocks = (
 
 async function slackNotificationPayload(channelId, userId, responseType) {
   const [chqNodeResponse, marketplaceRank] = await Promise.all([
-    axios.get(`https://www.drupal.org/api-d7/node/${config.chromaticDrupalOrgNid}.json`),
+    axios.get(`https://www.drupal.org/api-d7/node/${config.drupalOrganizationNodeId}.json`),
     getDrupalMarketplaceData(`${config.drupalOrgBaseUrl}${config.drupalOrgMarketplacePath}`),
   ]);
 
@@ -120,9 +130,19 @@ async function slackNotificationPayload(channelId, userId, responseType) {
       },
     ],
   };
-  console.log(payload.attachments[0]);
   return payload;
 }
+
+const slackErrorPayload = (channelId, userId, responseType, error) => {
+  const payload = {
+    token: config.slackBotToken,
+    channel: channelId,
+    user: userId,
+    response_type: responseType,
+    text: `An unknown error has occurred: \`${error}\``,
+  };
+  return payload;
+};
 
 // Listen for a slash command.
 app.command('/dorank', async ({ command, ack, respond }) => {
@@ -130,27 +150,30 @@ app.command('/dorank', async ({ command, ack, respond }) => {
   await ack();
 
   console.log(command);
+  let payload;
   try {
-    const payload = await slackNotificationPayload(command.channel_id, command.user_id, 'ephemeral');
+    payload = await slackNotificationPayload(command.channel_id, command.user_id, 'ephemeral');
     return await respond(payload);
   } catch (error) {
     console.error(error);
+    payload = await slackErrorPayload(command.channel_id, command.user_id, 'ephemeral', error);
+    return respond(payload);
   }
 });
 
 // This endpoint is triggered by a non-Slack event like a Jenkins job for a
-// weekly notification in the configured announcements channel.
+// weekly notification in the configured channel.
 receiver.app.post('/triggers', async (request, response, next) => {
-  if (request.query.token !== config.chromaticToken) {
+  if (request.query.token !== config.orgToken) {
     response.status(403);
     return response.send();
   }
   try {
     const channelId = config.debugMode ?
       config.channels.sandboxId :
-      config.channels.openSourceId;
+      config.channels.defaultChannelId;
     const payload = await slackNotificationPayload(channelId, null, 'in_channel');
-    return app.client.chat.postMessage(payload);
+    app.client.chat.postMessage(payload);
   } catch (error) {
     console.error(error);
   }
@@ -159,7 +182,8 @@ receiver.app.post('/triggers', async (request, response, next) => {
 });
 
 receiver.app.get('/', (_, res) => {
-  res.status(200).send(); // respond 200 OK to the default health check method
+  // Respond 200 OK to the default health check method.
+  res.status(200).send();
 });
 
 (async () => {
